a661fc87672cc0893ae66e1dd3d42f457d4d0450
Roni Laukkarinen <roni@dude.fi>
2021-03-29 19:10:07 +0300

    Add get air button

--- content/themes/airwptheme/front-page.php
+++ content/themes/airwptheme/front-page.php
@@ -18,24 +18,10 @@ get_header(); ?>
 
 <main class="site-main">
 
-  <section class="block block-hero-fp">
-    <div class="container">
-      <h1>Speed up your WordPress development</h1>
-      <p>Air-light WordPress Starter  Theme</p>
-
-      <div class="featured">
-        <p><span>As featured in</span></p>
-        <ul>
-          <li><a class="hongkiat" aria-label="Hongkiat: Fresh Resources for Web Designers and Developers (December 2017)" href="https://www.hongkiat.com/blog/designers-developers-monthly-12-2017/"><?php include get_theme_file_path( '/svg/logo-hongkiat.svg' ); ?></a></li>
-          <li><a class="speckyboy" aria-label="Speckyboy Design Magazine: The Top Starter & Barebone Themes for WordPress Development" href="https://speckyboy.com/wordpress-starter-themes/"><?php include get_theme_file_path( '/svg/logo-speckyboy.svg' ); ?></a></li>
-          <li><a class="wplift" aria-label="WPLift: 40+ Blank WordPress Starter Themes & Frameworks to Help You Kick-start Your Entrepreneurial Journey" href="https://wplift.com/wordpress-starter-themes"><?php include get_theme_file_path( '/svg/logo-wplift.svg' ); ?></a></li>
-          <li><a class="wpbeginner" aria-label="WPBeginner:
-19 Best WordPress Starter Themes for Developers in 2021" href="https://www.wpbeginner.com/wp-themes/21-best-wordpress-starter-themes-for-developers/"><?php include get_theme_file_path( '/svg/logo-wpbeginner.svg' ); ?></a></li>
-          <li><a class="themessearch" aria-label="themessearch: Top 10+ Best Free Starter Themes for WordPress Theme Development" href="https://www.wpbeginner.com/wp-themes/21-best-wordpress-starter-themes-for-developers/"><?php include get_theme_file_path( '/svg/logo-themessearch.svg' ); ?></a></li>
-        </ul>
-      </div>
-    </div>
-  </section>
+  <?php
+    get_template_part( 'template-parts/hero-fp' );
+    get_template_part( 'template-parts/introduction' );
+  ?>
 
 </main>
 
---
+++ content/themes/airwptheme/template-parts/introduction.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package airwptheme
+ */
+
+?>
+<section class="block block-introduction">
+  <div class="container">
+    <h2>Why Air?</h2>
+
+    <div class="cols">
+      <div class="col">
+        <h3>Fully accessible</h3>
+        <p>Accessibility (a11y) has always been one of our core values when it comes to development. Air-light has built in accessible navigation patterns for screen readers but also tools for accessible development like <a href="https://github.com/YozhikM/stylelint-a11y">stylelint-a11y</a>. Say no to inaccessible web!</p>
+
+        <p class="button-wrapper">
+          <a class="button" href="https://github.com/digitoimistodude/air-light#accessibility">
+            Read more about accessibility in Air
+          </a>
+        </p>
+
+        <h3>No CSS frameworks languages</h3>
+        <p>Forget Bootstrap, Bulma, Foundation and Tailwind. CSS is not hard. Air-light has only the necessary elements like typography, forms and buttons, nothing excess. Create your own CSS framework with Air-light!</p>
+
+        <p class="button-wrapper">
+          <a class="button" href="https://github.com/digitoimistodude/air-light#accessibility">
+            Read more about Air CSS
+          </a>
+        </p>
+
+        <h3>No PHP templating languages</h3>
+        <p>No "fancy" templating in the way. Code WordPress like you are used to with traditional PHP.</p>
+
+        <p class="button-wrapper">
+          <a class="button" href="https://github.com/digitoimistodude/air-light#accessibility">
+            Read more about Air markup
+          </a>
+        </p>
+
+        <h3>No features you never use</h3>
+        <p>We have disabled widgets, post formats, Jetpack support, (Threaded) comments, Underscores Template tags and sidebar from our theme. These are almost never needed and just add extra weight.</p>
+
+        <p class="button-wrapper">
+          <a class="button" href="https://github.com/digitoimistodude/air-light#accessibility">
+            Read more about Air features
+          </a>
+        </p>
+      </div>
+    </div>
+
+  </div>
+</section>
