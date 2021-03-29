6369d72babc22cfbf31d8923635f129138f0ea44
Roni Laukkarinen <roni@dude.fi>
2021-03-29 16:22:27 +0300

    First commit - project started

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+set :application, "airwptheme"
+set :repo_url,  "git@github.com:digitoimistodude/airwptheme.git"
+set :branch, :master
+set :log_level, :debug
+set :linked_files, %w{.env}
+set :linked_dirs, %w{media}
+set :composer_install_flags, '--no-dev --prefer-dist --no-scripts --optimize-autoloader'
+set :composer_roles, :all
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # This task is required by Capistrano but can be a no-op
+      # Your restart mechanism here, for example:
+      # execute :service, :nginx, :reload
+    end
+  end
+
+end
