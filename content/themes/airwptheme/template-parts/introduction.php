c2a7bb279d181f30ab7760a85553d653f55048c7
Roni Laukkarinen <roni@dude.fi>
2021-03-29 20:36:51 +0300

    Add more features and icons

--- content/themes/airwptheme/template-parts/introduction.php
+++ content/themes/airwptheme/template-parts/introduction.php
@@ -10,38 +10,88 @@
 
     <div class="cols">
       <div class="col">
-        <h3>Fully accessible</h3>
-        <p>Accessibility (a11y) has always been one of our core values when it comes to development. Air-light has built in accessible navigation patterns for screen readers but also tools for accessible development like <a href="https://github.com/YozhikM/stylelint-a11y">stylelint-a11y</a>. Say no to inaccessible web!</p>
+        <h3>
+          <?php include get_theme_file_path( '/svg/github.svg' ); ?>
+          Open source
+        </h3>
+        <p>As Air-light is a open source project it will get weekly updates. You can contribute your own changes and follow the roadmap online.</p>
       </div>
 
       <div class="col">
-        <h3>Ultra minimal</h3>
-        <p>CSS weights only 13 KB and JavaScript 7.8 KB. This means faster loading times, because there is no excess code.</p>
+        <h3>
+          <?php include get_theme_file_path( '/svg/a11y.svg' ); ?>
+          Accessible &amp; WCAG 2.0 AAA Compliant
+        </h3>
+        <p>Air-light has built in accessible navigation patterns for screen readers but also tools for accessible development. Say no to inaccessible web!</p>
       </div>
 
       <div class="col">
-        <h3>Custom Post Support</h3>
-        <p>CPTs and taxonomies are our daily bread. That's why they are included so that adding them is easiest thing ever.</p>
+        <h3>Small file sizes</h3>
+        <p>CSS weights only 13 KB and JavaScript 7.8 KB. This means faster loading times, because there is no excess code. With Air you build speed in mind.</p>
       </div>
 
       <div class="col">
-        <h3>Full support for ACF and Gutenberg</h3>
+        <h3>
+          <?php include get_theme_file_path( '/svg/wordpress.svg' ); ?>
+          Custom Post Type Support
+        </h3>
+        <p>CPTs and taxonomies are our daily bread. That's why they are included so that adding them is easiest thing ever. No more CPT generators!</p>
+      </div>
+
+      <div class="col">
+        <h3>
+          <?php include get_theme_file_path( '/svg/acf-gutenberg.svg' ); ?>
+          Full support for ACF and Gutenberg
+        </h3>
         <p>With Air it's very easy to create custom elements with Advanced Custom Fields and Gutenberg. The theme even has a boilerplate for creating modular flexible elements.</p>
       </div>
 
       <div class="col">
-        <h3>No CSS frameworks languages</h3>
-        <p>Forget Bootstrap, Bulma, Foundation and Tailwind. CSS is not hard. Air-light has only the necessary elements like typography, forms and buttons, nothing excess. Create your own CSS framework with Air-light!</p>
+        <h3>
+          <?php include get_theme_file_path( '/svg/sass.svg' ); ?>
+          Modern CSS with PostCSS and SCSS without any bloat
+        </h3>
+        <p>Forget Bootstrap, Bulma, Foundation and Tailwind. CSS is not hard. Air-light has only the necessary elements like typography, forms and buttons, nothing excess. Create your own SCSS framework with Air-light!</p>
       </div>
 
       <div class="col">
-        <h3>No PHP templating languages</h3>
-        <p>No "fancy" templating in the way. Code WordPress like you are used to with traditional PHP. We only used Namespaced PHP, but that's it!</p>
+        <h3>
+          <?php include get_theme_file_path( '/svg/php.svg' ); ?>
+          Modern PHP without templating languages
+        </h3>
+        <p>No "fancy" templating languages in the way. There's no need to reinvent the bicycle. Code WordPress like you are used to with traditional PHP. We only used Namespaced PHP, but that's it!</p>
       </div>
 
       <div class="col">
-        <h3>No features you never use</h3>
+        <h3>
+          <?php include get_theme_file_path( '/svg/trash.svg' ); ?>
+          No features you never use
+        </h3>
         <p>We have disabled widgets, post formats, Jetpack support, (Threaded) comments, Underscores Template tags and sidebar from our theme. These are almost never needed and just add extra weight.</p>
+      </div>
+
+      <div class="col">
+        <h3>
+          <?php include get_theme_file_path( '/svg/google.svg' ); ?>
+          SEO friendly
+        </h3>
+        <p>Google and search engines love WordPress. As Air-light is built using the latest standards, it is the best fit for search engine optimized websites. Yoast or SEO Framework, it's your call!</p>
+      </div>
+
+      <div class="col">
+        <h3>
+          <?php include get_theme_file_path( '/svg/translation.svg' ); ?>
+          Translation-ready
+        </h3>
+        <p>No untranslatable strings! Air-light comes with Finnish and English built in but it is very easy to add your own language. You can use po-files or ask_e function with Polylang.</p>
+      </div>
+
+      <div class="col">
+        <h3>
+          <?php include get_theme_file_path( '/svg/gulp.svg' ); ?>
+          Modern JavaScript
+        </h3>
+        <p>Like Vue.js or Mithril? With Air-light it's easy to use any modern JavaScript frameworks. JS is built with gulp and webpack and npm is baked-in so only sky is the limit!</p>
       </div>
     </div>
 
