874325963bfb41888f686472c24c41fc5fae6372
Roni Laukkarinen <roni@dude.fi>
2021-03-29 19:22:14 +0300

    Add introduction

--- content/themes/airwptheme/template-parts/introduction.php
+++ content/themes/airwptheme/template-parts/introduction.php
@@ -12,39 +12,36 @@
       <div class="col">
         <h3>Fully accessible</h3>
         <p>Accessibility (a11y) has always been one of our core values when it comes to development. Air-light has built in accessible navigation patterns for screen readers but also tools for accessible development like <a href="https://github.com/YozhikM/stylelint-a11y">stylelint-a11y</a>. Say no to inaccessible web!</p>
+      </div>
 
-        <p class="button-wrapper">
-          <a class="button" href="https://github.com/digitoimistodude/air-light#accessibility">
-            Read more about accessibility in Air
-          </a>
-        </p>
+      <div class="col">
+        <h3>Ultra minimal</h3>
+        <p>CSS weights only 13 KB and JavaScript 7.8 KB. This means faster loading times, because there is no excess code.</p>
+      </div>
 
+      <div class="col">
+        <h3>Custom Post Support</h3>
+        <p>CPTs and taxonomies are our daily bread. That's why they are included so that adding them is easiest thing ever.</p>
+      </div>
+
+      <div class="col">
+        <h3>Full support for ACF and Gutenberg</h3>
+        <p>With Air it's very easy to create custom elements with Advanced Custom Fields and Gutenberg. The theme even has a boilerplate for creating modular flexible elements.</p>
+      </div>
+
+      <div class="col">
         <h3>No CSS frameworks languages</h3>
         <p>Forget Bootstrap, Bulma, Foundation and Tailwind. CSS is not hard. Air-light has only the necessary elements like typography, forms and buttons, nothing excess. Create your own CSS framework with Air-light!</p>
+      </div>
 
-        <p class="button-wrapper">
-          <a class="button" href="https://github.com/digitoimistodude/air-light#accessibility">
-            Read more about Air CSS
-          </a>
-        </p>
-
+      <div class="col">
         <h3>No PHP templating languages</h3>
-        <p>No "fancy" templating in the way. Code WordPress like you are used to with traditional PHP.</p>
+        <p>No "fancy" templating in the way. Code WordPress like you are used to with traditional PHP. We only used Namespaced PHP, but that's it!</p>
+      </div>
 
-        <p class="button-wrapper">
-          <a class="button" href="https://github.com/digitoimistodude/air-light#accessibility">
-            Read more about Air markup
-          </a>
-        </p>
-
+      <div class="col">
         <h3>No features you never use</h3>
         <p>We have disabled widgets, post formats, Jetpack support, (Threaded) comments, Underscores Template tags and sidebar from our theme. These are almost never needed and just add extra weight.</p>
-
-        <p class="button-wrapper">
-          <a class="button" href="https://github.com/digitoimistodude/air-light#accessibility">
-            Read more about Air features
-          </a>
-        </p>
       </div>
     </div>
 
