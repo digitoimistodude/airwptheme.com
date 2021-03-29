990f098c3f0fea5e753c41bcff7006e545adb4f1
Roni Laukkarinen <roni@dude.fi>
2021-03-29 18:21:29 +0300

    Hero and featured logos

--- content/themes/airwptheme/template-parts/header/branding.php
+++ content/themes/airwptheme/template-parts/header/branding.php
@@ -13,9 +13,7 @@ $description = get_bloginfo( 'description', 'display' );
 <div class="site-branding">
 
   <p class="site-title">
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-      <span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
-      <?php include get_theme_file_path( THEME_SETTINGS['logo'] ); ?>
+      <span class="label"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php include get_theme_file_path( '/svg/logo.svg' ); ?> WordPress Starter Theme</label></a> <span class="by">by</span> <a href="https://www.dude.fi"><span class="screen-reader-text">DUDE</span> <?php include get_theme_file_path( '/svg/logo-dude.svg' ); ?></a>
     </a>
   </p>
 
